5ba565f9346f7fd29b0d52412fcc17cc98deb882
Apocalypse <k023g0063@g.neec.ac.jp>
2025-05-12 16:30:44 +0900

    クロスヘアとRayによる弾の発射方向の追加

--- GameProject/GameScene/Object/Crosshair/Crosshair.cpp
+++ GameProject/GameScene/Object/Crosshair/Crosshair.cpp
@@ -1,5 +1,9 @@
 #include "Crosshair.h"
 
+#include "Collision/CollisionManager.h"
+#include "GameScene/Object/Player/Player.h"
+#include "Type/Singleton.h"
+
 void Crosshair::Initialize() {
     Object::Initialize();
 
@@ -15,9 +19,36 @@ void Crosshair::Initialize() {
 void Crosshair::Update() {
     Object::Update();
 
+    UpdateRay();
+
     sprite_->Update();
 }
 
 void Crosshair::Draw() {
     sprite_->Draw();
+}
+
+void Crosshair::SetPlayer(Player* _player) {
+    pPlayer_ = _player;
+
+    ray_ = std::make_unique<Collision::Ray>(Adaptor(pPlayer_->GetTransform().translate), Adaptor(pPlayer_->GetTransform().rotate), REACH);
+}
+
+void Crosshair::UpdateRay() {
+    // Update
+    if (pPlayer_ == nullptr)return;
+    ray_->SetOrigin(Adaptor(pPlayer_->GetTransform().translate));
+    ray_->SetDirection(Adaptor(pPlayer_->GetTransform().rotate));
+
+    // Detection
+    auto data = Singleton<Collision::Manager>::GetInstance()->RayCast(ray_.get()); //
+    if (data.pair.first.empty() || data.pair.second.empty()) // No hit
+    if ((Adaptor(data.hitPoint) - pPlayer_->GetTransform().translate).Length() <= 1.f)return; // Prevents penetration into buildings
+
+    transform_.translate = Adaptor(data.hitPoint);
+}
+
+void Crosshair::NoHit() {
+    transform_.translate = pPlayer_->GetTransform().translate + (pPlayer_->GetTransform().rotate * REACH);
+    transform_.rotate = pPlayer_->GetTransform().rotate;
 }
--- GameProject/GameScene/Object/Crosshair/Crosshair.h
+++ GameProject/GameScene/Object/Crosshair/Crosshair.h
@@ -6,10 +6,24 @@ class Player;
 
 class Crosshair final : public Object{
     std::unique_ptr<Sprite> sprite_;
+    std::unique_ptr<Collision::Ray> ray_;
 
+    Player* pPlayer_ = nullptr;
+
+    const float REACH = 100.f;
 public:
     void Initialize() override;
     void Update() override;
     void Draw() override;
+
+    void SetPlayer(Player* _player);
+
+    Vector3 GetAimPosition() const {
+        return transform_.translate;
+    }
+
+private:
+    void UpdateRay();
+    void NoHit();
 };
 
