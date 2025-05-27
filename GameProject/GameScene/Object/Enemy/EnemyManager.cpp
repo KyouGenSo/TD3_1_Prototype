754b810b849da7949bd1d66211fca6302d171a6d
fukumotokiraa <122710kisose@gmail.com>
2025-05-27 16:46:19 +0900

    10waveまで作成

--- GameProject/GameScene/Object/Enemy/EnemyManager.cpp
+++ GameProject/GameScene/Object/Enemy/EnemyManager.cpp
@@ -33,17 +33,25 @@ void EnemyManager::Initialize(Object* player, Object* castle)
 
 
 
-    InitializeWaveFile("0101n");
-    InitializeWaveFile("0102n");
-    InitializeWaveFile("0103n");
-    //InitializeWaveFile("0103f");
+    InitializeWaveFile("00n");
+    InitializeWaveFile("01n");
+    InitializeWaveFile("02n");
+    InitializeWaveFile("03f");
+    InitializeWaveFile("04n");
+    InitializeWaveFile("04f");
+    InitializeWaveFile("05n");
+    InitializeWaveFile("05f");
+    InitializeWaveFile("06n");
+    InitializeWaveFile("06b");
+    InitializeWaveFile("07f");
+    InitializeWaveFile("07b");
+    InitializeWaveFile("08f");
+    InitializeWaveFile("08b");
+    InitializeWaveFile("09n");
+    InitializeWaveFile("10n");
+    InitializeWaveFile("10f");
+    InitializeWaveFile("10b");
 
-    //InitializeWaveFile("0201n");
-    //InitializeWaveFile("0202n");
-    //InitializeWaveFile("0202b");
-    //InitializeWaveFile("0203n");
-    //InitializeWaveFile("0203f");
-    //InitializeWaveFile("0203b");
 
     TurnControl();
     turnProgress++;
