124b42beddcbf22f2b8fb418037c301a346e0f43
Souto-Naitou <k023g0031@g.neec.ac.jp>
2025-05-29 18:49:35 +0900

    change: ポーズメニュー機能の改善と修正
    - `BounceEnemy::OnCollision`メソッドのコードを削除し、敵が死亡した際の処理を変更しました。
    
    - `GUI_PauseMenu.cpp`に新しいヘッダーファイルを追加し、`OnNotify`メソッドにポーズメニューのオープン、クローズ、トグルに関連する処理を追加しました。
    
    - `ShowPauseMenu`メソッド内で背景色を設定し、ポーズメニューの表示に関する処理を強化しました。
    
    - `imgui.ini`ファイルに新しいウィンドウ設定を追加し、複数のウィンドウの位置やサイズを指定しました。
    
    - バイナリファイルやJSONファイルの変更は軽微です。

--- GameProject/GameUI/PauseMenu/GUI_PauseMenu.cpp
+++ GameProject/GameUI/PauseMenu/GUI_PauseMenu.cpp
@@ -1,5 +1,12 @@
 #include "GUI_PauseMenu.h"
 
+#include <GameUI/ColorResolver/ColorResolver.h>
+#include <GameSystem/DeltaTimeManager/DeltaTimeManager.h>
+#include <string>
+#include <Math/NiVec4.h>
+#include <Type/NiGui_Enum.h>
+#include <Utility/Adaptor.h>
+#include <GameUI/ColorName/ColorName.h>
 #include <NiGui.h>
 
 void GUI_PauseMenu::Initialize()
@@ -17,16 +24,21 @@ void GUI_PauseMenu::Update()
 
 void GUI_PauseMenu::OnNotify(const std::string& _event)
 {
+    auto dtm = DeltaTimeManager::GetInstance();
     if (_event == "open_pause_menu")
     {
+        dtm->SetDeltaTime(1, 0.0f);
         showPauseMenu_ = true;
     }
     else if (_event == "close_pause_menu")
     {
+        dtm->SetDeltaTime(1, 1.0f / 60.0f);
         showPauseMenu_ = false;
     }
     else if (_event == "toggle_pause_menu")
     {
+        if (showPauseMenu_) dtm->SetDeltaTime(1, 1.0f / 60.0f);
+        else dtm->SetDeltaTime(1, 0.0f);
         showPauseMenu_ = !showPauseMenu_;
     }
 }
@@ -36,15 +48,22 @@ void GUI_PauseMenu::ShowPauseMenu()
     auto center = NiGui_StandardPoint::Center;
     auto confirm = NiGui_ButtonState::Confirm;
 
+    NiVec4 bgcolor = NiUtil::Adaptor(ColorResolver::GetInstance()->Resolve(ColorName::Background).toVector4());
+    NiGui::BeginDiv("PauseMenu_Background", "white.png", bgcolor, {}, { 1600,900 }, center, center);
+    NiGui::EndDiv();
+
+    auto dtm = DeltaTimeManager::GetInstance();
 
     if (NiGui::BeginDiv("PauseMenu", "white.png", NiGui::BLACK, {}, { 250, 250 }, center, center))
     {
         if (NiGui::Button("Resume", "white.png", NiGui::WHITE, { 0.0f, -50.0f }, { 150.0f, 50.0f }, {}, center, center) == confirm)
         {
+            dtm->SetDeltaTime(1, 1.0f / 60.0f);
             showPauseMenu_ = false;
         }
         if (NiGui::Button("Exit", "white.png", NiGui::WHITE, { 0.0f, 50.0f }, { 150.0f, 50.0f }, {}, center, center) == confirm)
         {
+            dtm->SetDeltaTime(1, 1.0f / 60.0f);
             showPauseMenu_ = false;
         }
     }
